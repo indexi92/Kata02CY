77ac0d90bf6373e7bd8af1ca19d3da515d9b0f77
indexi92 <klicicervin@hotmail.com>
2022-05-09 15:12:06 +0200

    intercept evol

--- cypress/integration/apiIntercept.spec.js
+++ cypress/integration/apiIntercept.spec.js
@@ -1,10 +1,21 @@
 /// <reference types="cypress" />
 
 describe("apiTest", () => {
-    before(() => {
-      cy.visit("/");
-      cy.url().should("eq", Cypress.config().baseUrl);
+  before(() => {
+    cy.visit("/");
+    cy.url().should("eq", Cypress.config().baseUrl);
+  });
+
+  it("intercept", () => {
+    cy.intercept({
+      url: "**/view**",
+      method: "POST",
+    }).as("Phone");
+    cy.get('[class="hrefch"]').eq(0).click();
+    cy.wait("@Phone").then((intercept) => {
+      let request = intercept.request;
+      let response = intercept.response;
+      expect(response.statusCode).eq(200);
     });
-  
-    it("intercept", () => {
-    });
+  });
+});
